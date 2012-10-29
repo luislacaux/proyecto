2a364a2fb43aa506d84ec45b163aadb54ec6d542
Nacho <Nacho@Nacho-PC>
2012-10-29 00:46:48 -0300

    nueva estructura

---
+++ Profesor/js/script.js
@@ -0,0 +1,62 @@
+var num_campos=3;//numero de campos input por fila;  esta variable debería actializarse dinamicamente, debería leer el número de inputs, esta te la dejo de tarea
+
+function nuevo(){
+    //alert("nuevo")
+    $("#tablaUsuarios").append("<tr>"+
+        "<td><input name='prod[]' type='text' size='15' placeholder='Ingrese Pregunta' required/></td>"+
+        "<td><input name='cant[]' type='text' size='10' placeholder='Ingrese Solucion' required/></td>"+
+        "<td><input name='desc[]' type='text' size='10' placeholder='Ingrese Unidad' required/></td>"+
+        "<td><input type='button' value='Eliminar' onclick='eliminar(this)' class='btn btn-danger eliminar'></td>"+
+        "</tr>")
+}
+
+//Ctrl + Shift + j     para ver la consola (consejo: usa chrome)
+function guardar(e){
+    //NOTA: Recuerda SIMPRE VALIDAR los campos del formulario del lado del servidor y el cliente.
+    e.preventDefault()
+    var res = $("form#f").serializeArray();
+    console.log(res)//descomenta esta linea y mira la consola, así llegan nuestros datos,
+    var npreg = res.length;
+    var cont = 0;
+    $("#result").empty();//limpiar la caja
+    var preguntas = [];//un array que contendra a los arrays (filas o productos)
+    var pregunta = [];//array para cada una de las filas 
+    for (i=0;i<npreg;i++){//debe imprimer de 4 en 4 porque estan todos los inputs en un solo array
+  
+        $("#result").append(res[i].value);
+        
+        //VALIDAR SIEMPRE
+        pregunta.push(res[i].value);//esta linea agrega los datos a nuestro array
+        
+        if(cont < num_campos-1 ){
+            $("#result").append(", ");
+            cont++;
+        }
+        else{
+            $("#result").append(" <br />");
+            preguntas.push(pregunta);
+            pregunta = [];
+            cont=0;
+        }
+    }
+    console.log(preguntas);//productos es un array que contiene n arrays (n productos)
+
+   
+}
+
+function eliminar(e){
+    $(e).parent().parent().fadeOut(400).remove();
+/**
+     * el boton eliminar esta jerarquicamente asi:
+     *      form > table > tr > td > input.eliminar
+     * por esta razon se debe subir dos posiciones 
+     */
+}
+
+function iniciar(){
+    //alert("Hola Jquery")
+    $("#newp").on("click",nuevo)//si dan click a #newp ejecutar nuevo
+    $("#guardar").on("click",guardar)
+}
+
+$(document).on("ready",iniciar)//cuando el document esté 'ready' ejecutar la funcion 'iniciar'
