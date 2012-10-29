9656f09a5db7f26f98199cdacd95bd2f2e2f6111
Luis <Luis@Luis-PC>
2012-10-29 00:43:21 -0300

    asdasdasd

--- webservices/mysql.php
+++ webservices/mysql.php
@@ -1,26 +1,15 @@
 <?php
 
 function conectarDB(){
-	/* Database config */
-
 	$db_host		= 'localhost';
 	$db_user		= 'root';
 	$db_pass		= '123';
-	$db_database	= 'quimica'; 
-	
-	
-	
+	$db_database	        = 'quimica'; 
 	$conexion = mysql_connect($db_host,$db_user,$db_pass)or die(mysql_error());
 	mysql_select_db($db_database,$conexion)or die(mysql_error());
-    
-    
-	return $conexion;
+    	return $conexion;
 }
-
 function desconectarDB(){
 	mysql_close()or die(mysql_error());
 }
-
-
-
 ?>
